a0ae4769108b84d86ab4160703636c3488b8d9e6
= <=>
2025-05-01 13:58:09 -0400

    Mapbox working styling isnt styling tho

--- VenueFrontend/src/app/components/map-view/map-view.component.ts
+++ VenueFrontend/src/app/components/map-view/map-view.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewInit } from '@angular/core';
+import { environment } from '../../../enviroments/enviroments';
+import { Venue } from '../../models/venues';
+import { VenueService } from '../../services/venue.service';
+import * as mapboxgl from 'mapbox-gl';
+import "mapbox-gl/dist/mapbox-gl.css";
 @Component({
   selector: 'app-map-view',
   imports: [],
   templateUrl: './map-view.component.html',
   styleUrl: './map-view.component.css'
 })
-export class MapViewComponent {
+export class MapViewComponent implements AfterViewInit {
 
+  map!: mapboxgl.Map;
+
+  ngAfterViewInit(): void {
+
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/kevinch65/cma1sg24e00b701s54z9b8jl3',
+      center: [-79.3832, 43.6532], 
+      zoom: 12,
+      accessToken: environment.mapboxToken
+    });
+  }
 }
---
+++ VenueFrontend/src/enviroments/enviroments.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    mapboxToken: 'pk.eyJ1Ijoia2V2aW5jaDY1IiwiYSI6ImNtYTFxbjBmNDF6cjkyanE1OWM4YXN4OGoifQ.Srrd28DM_0w4icC0eO63jg'
+}
