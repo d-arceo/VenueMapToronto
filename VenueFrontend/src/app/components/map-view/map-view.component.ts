de3f63d46d9d65ce3f0125628374804150787fc9
d-arceo <131067966+d-arceo@users.noreply.github.com>
2025-05-09 18:57:26 -0400

    Remove building labels (#11)
    * Created test.py to test eventbrite token in API
    
    * Removed building labels from map
    
    * Moved eventbrite API keys into env. Deleted test.py.

--- VenueFrontend/src/app/components/map-view/map-view.component.ts
+++ VenueFrontend/src/app/components/map-view/map-view.component.ts
@@ -18,7 +18,7 @@ export class MapViewComponent implements AfterViewInit {
 
     this.map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/arceodan/cmad61o0800ar01s81b842d6a', // basic 3D style
+      style: 'mapbox://styles/arceodan/cmagxvwov00mo01s8ap0n6fg6', // no building labels style
       center: [-79.483006, 43.665544], 
       zoom: 18, 
       pitch: 45, // tilts map 45 degrees
--- VenueFrontend/src/enviroments/enviroments.ts
+++ VenueFrontend/src/enviroments/enviroments.ts
@@ -1,4 +1,8 @@
 export const environment = {
     production: false,
-    mapboxToken: 'pk.eyJ1IjoiYXJjZW9kYW4iLCJhIjoiY205cm91dzRuMDNsbDJscTJqNHJ0bGhwbSJ9.XNBPCiKK8SsdTeZxldYsdA'
+    mapboxToken: 'pk.eyJ1IjoiYXJjZW9kYW4iLCJhIjoiY205cm91dzRuMDNsbDJscTJqNHJ0bGhwbSJ9.XNBPCiKK8SsdTeZxldYsdA',
+    eventbriteAPIKey: "X6UCBX6MMVQFLLY54M",
+    eventbriteClientSecret: "RNTOIL6PNKPWCD2RNKJGTM377ACQ25NCNL23XXAGQOYQXOPFRW",
+    eventbritePrivateKToken: "5PVPALFHGVS3C5PJANFS",
+    eventbritePublicToken: "WONRXYIPFKNX5LQUEV4Q"
 }
