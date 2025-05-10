42d8a2576ec22dbe675e04d0e0cff16f2b05ebda
= <=>
2025-05-09 23:12:35 -0400

    added upcoming events on artist card, also added get Events in artist Service

--- VenueFrontend/src/app/services/artist.service.ts
+++ VenueFrontend/src/app/services/artist.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Artist } from '../models/artists';
 import { Observable, map } from 'rxjs';
+import { Event } from '../models/events';
+import { EventService } from './event.service';
 
 @Injectable({ providedIn: 'root' })
 export class ArtistService {
   private url = 'assets/data/artist.json';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private eventService: EventService) {}
 
   getArtists(): Observable<Artist[]> {
     return this.http.get<{ artists: Artist[] }>(this.url).pipe(
@@ -18,6 +20,14 @@ export class ArtistService {
   getArtistById(A_id: number): Observable<Artist | undefined> {
     return this.getArtists().pipe(
       map(artists => artists.find(artist => artist.A_id === A_id))
+    );
+  }
+
+  getEventsforArtist(artist: Artist): Observable<Event[]>{
+    return this.eventService.getAllEvents().pipe(
+      map(events =>
+        events.filter(event => event.lineUp.includes(artist.name))
+      )
     );
   }
 }
